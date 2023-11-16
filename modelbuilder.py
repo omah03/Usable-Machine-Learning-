bbf8535dec0bdadcb08144c358495cdd08a43fa0
SimmonsLZ0N <111833349+SimmonsLZ0N@users.noreply.github.com>
2023-11-16 22:19:32 +0100

    Everything is a class now

--- modelbuilder.py
+++ modelbuilder.py
@@ -1,52 +1,107 @@
+import torch
 import torch.nn as nn
+from torchinfo import summary
 
-class Modelbuilder(nn.Module):
+#Define characteristics for all the layers as lists of dictionaries
 
-    def __init__(self,numConvLayers,convChannels,kernel_size,stride,
-                 padding,activationFunction,learning_rate, 
-                 numEpochs,batch_size,inputsize=(28,28)):
-        super(Modelbuilder, self).__init__()
+activation_function = nn.Tanh
+conv_params = [
+    {"in_channels": 1,"out_channels": 16, "kernel_size": 8, "stride": 2, "padding": 2, "activation_function": activation_function},
+    {"in_channels": 16, "out_channels": 32, "kernel_size": 4, "stride": 2, "padding": 0, "activation_function": activation_function},
+]    
 
-        self.numConvLayers = numConvLayers 
-        self.convChannels = convChannels
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = padding
-        self.activationFunction = activationFunction
-        self.activationFunctions = [nn.Tanh,nn.ReLU,nn.Sigmoid]
-        self.learning_rate = learning_rate
-        self.numEpochs = numEpochs
-        self.batch_size = batch_size 
+maxpool_params = [
+    {"kernel_size":2,"stride":1},
+    {"kernel_size":2,"stride":1}
+]
+    
 
-        self.block1 = nn.Sequential(
-            nn.Conv2d(1,convChannels, kernel_size=kernel_size, stride=stride, padding=padding),
-            self.activationFunctions[self.activationFunctions.index(activationFunction)](),
-            nn.MaxPool2d(kernel_size=kernel_size,stride=stride,padding=padding)    
-        )
-     
+linear_params = [
+    {"in_features": 512, "out_features": 32, "activation_function": activation_function},
+    {"in_features": 32, "out_features": 10, "activation_function": activation_function},
+]
+
+"""
+Create blocks for each type of layer: Maxpool, Convolution and Linear. 
+Perhaps Maxpool and Conv can be stuck together since maxpool follows convolutional. 
+
+Conv2d amd linear each come integrated with their activation function since it's the same everywhere
+"""
+
+class Maxpool(nn.Module):
+    def __init__(self, kernel_size,stride):
+        super(Maxpool,self).__init__()
+        self.maxpool = nn.MaxPool2d(kernel_size,stride)
 
 
-        self.convolutional_layers = nn.Sequential(
-                nn.Conv2d(1, convChannels, kernel_size=kernel_size, stride=stride, padding=padding),
-                nn.Tanh(),
-                nn.MaxPool2d(kernel_size=kernel_size, stride=stride),
-                nn.Conv2d(convChannels, 32, kernel_size=4, stride=2, padding=0),
-                nn.Tanh(),
-                nn.MaxPool2d(kernel_size=2, stride=1),
-            )
-        self.linear_layers = nn.Sequential(
-            nn.Linear(512, 32),
-            nn.Tanh(),
-            nn.Linear(32, 10),
-        )
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, activation_function):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.activation = activation_function
 
-    def calculateOutputDims(self,widthInput,heightInput,kernelDim,stride,padding):
-        #only works with square kernels
-        widthOutput = (widthInput + 2*padding - (kernelDim-1)-1 // stride) + 1
-        heightOutput = (heightInput + 2*padding- (kernelDim-1)-1 // stride) + 1
-        return widthOutput,heightOutput
+    def forward(self, x):
+        return self.activation(self.conv(x))
+
+class LinearBlock(nn.Module):
+    def __init__(self, in_features, out_features, activation_function):
+        super(LinearBlock, self).__init__()
+        self.linear = nn.Linear(in_features, out_features)
+        self.activation = activation_function
+
+    def forward(self, x):
+        return self.activation(self.conv(x))
+
+
+
+class ModelBuilder(nn.Module):
+    def __init__(self, conv_params, linear_params, max_pool_params, input_size=(28,28)):
+        super(ModelBuilder, self).__init__()
+        assert len(conv_params) == len(max_pool_params)
+
+        self.conv_layers = nn.ModuleList()
+        in_channels = 1  #Grescale first, scaled up through channels latere
+        w, h = input_size
+        print(f'width:{w}, height:{w}')
+        for conv_param, max_pool_param in zip(conv_params, max_pool_params):
+
+            self.conv_layers.append(ConvBlock(in_channels, **conv_param))
+            kernel_dim = conv_param.get('kernel_size')
+            stride = conv_param.get('stride')  
+            padding = conv_param.get('padding')
+            w,h = self.calculate_output_dims(w, h, kernel_dim, stride, padding)
+
+            kernel_dim = max_pool_param.get('kernel_size')
+            stride = max_pool_param.get('stride')  
+            padding = max_pool_param.get('padding')
+
+            self.conv_layers.append(Maxpool(**max_pool_param))
+            self.calculate_output_dims(w, h, kernel_dim, stride, padding)
+
+            in_channels = conv_param['out_channels']
+
+        # Calculate flattened size based on the last values of w and h
+        flattened = in_channels * w * h
+        print(flattened)
+
+        self.linear_layers = nn.ModuleList()
+        in_features = flattened
+        for params in linear_params:
+            self.linear_layers.append(LinearBlock(in_features, **params))
+          
+
+    def calculate_output_dims(self,width_input,height_input,kernel_dim,stride,padding):
+        
+        #only works with square kernels for now
+
+        width_output = (width_input + 2*padding - (kernel_dim-1)-1 // stride) + 1
+        height_output = (height_input + 2*padding- (kernel_dim-1)-1 // stride) + 1
+        return width_output,height_output
+
 
    
+
+    
     def forward(self, x):
         x = self.convolutional_layers(x)
         x = x.view(x.size(0), -1)
@@ -54,6 +109,6 @@ class Modelbuilder(nn.Module):
         return x
 
 
-
-
+model = ModelBuilder(conv_params=conv_params,linear_params=linear_params,max_pool_params=maxpool_params)
+print(summary(model))
 
