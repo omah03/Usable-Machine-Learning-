b6a284687aec402860d7103ae319df90faf88d97
Omar <omarhany.212@yahoo.com>
2023-11-18 17:33:09 +0100

    Big changes
    Added more functionality, fixed errors and formulas

--- ml_utils/modelbuilder.py
+++ ml_utils/modelbuilder.py
@@ -15,8 +15,26 @@ THE BLOCK:
         Activation function
         MaxPool layer
 """
+
+activation_function = nn.Tanh
+
+conv_params = [
+    {"in_channels": 1,"out_channels": 16, "kernel_size": 8, "stride": 2, "padding": 2, "activation_function": activation_function},
+    {"in_channels": 16, "out_channels": 32, "kernel_size": 4, "stride": 2, "padding": 0, "activation_function": activation_function},
+]    
+
+maxpool_params = [
+    {"kernel_size":2,"stride":1},
+    {"kernel_size":2,"stride":1}
+]
+    
+
+linear_params = [
+    {"in_features": 512, "out_features": 32, "activation_function": activation_function},
+    {"in_features": 32, "out_features": 10, "activation_function": activation_function},
+]
 class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, activation_function):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, activation_function,maxpool_params):
         super(ConvBlock, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
         self.activation = activation_function
@@ -34,21 +52,21 @@ class LinearBlock(nn.Module):
         self.activation = activation_function
 
     def forward(self, x):
-        return self.activation(self.conv(x))
-
+        return self.activation(self.linear(x))
 
 class ModelBuilder(nn.Module):
     def __init__(self, conv_params, linear_params, max_pool_params, input_size=(28,28)):
         super(ModelBuilder, self).__init__()
         assert len(conv_params) == len(max_pool_params)
+        # Initialize with at least one convolutional block
+        self.conv_layers = nn.ModuleList([ConvBlock(1, **conv_params[0], maxpool_params=max_pool_params[0])])
 
         self.conv_layers = nn.ModuleList()
         in_channels = 1  #Grescale first, scaled up through channels latere
         w, h = input_size
-        print(f'width:{w}, height:{w}')
-        for conv_param, max_pool_param in zip(conv_params, max_pool_params):
-
-            self.conv_layers.append(ConvBlock(in_channels, **conv_param))
+        print(f'width:{w}, height:{h}')
+        for conv_param, max_pool_param in zip(conv_params, max_pool_params,):
+            self.conv_layers.append(ConvBlock(in_channels, **conv_param,maxpool_params=max_pool_param))
             kernel_dim = conv_param.get('kernel_size')
             stride = conv_param.get('stride')  
             padding = conv_param.get('padding')
@@ -60,7 +78,6 @@ class ModelBuilder(nn.Module):
 
             in_channels = conv_param['out_channels']
 
-        # Calculate flattened size based on the last values of w and h
         flattened = in_channels * w * h
 
         self.linear_layers = nn.ModuleList()
@@ -70,10 +87,10 @@ class ModelBuilder(nn.Module):
             in_features = params ['out_features']
           
 
-    def calculate_output_dims(self,width_input,height_input,kernel_dim,stride,padding):      
+    def calculate_output_dims(self,width_input,height_input,kernel_size,stride,padding):      
         #only works with square kernels for now
-        width_output = (width_input + 2*padding - (kernel_dim-1)-1 // stride) + 1
-        height_output = (height_input + 2*padding- (kernel_dim-1)-1 // stride) + 1
+        width_output = (width_input - kernel_size + 2 * padding )// stride + 1
+        height_output = (height_input - kernel_size + 2 * padding) // stride + 1
         return width_output,height_output
  
     def forward(self, x):
@@ -83,6 +100,79 @@ class ModelBuilder(nn.Module):
         for layer in self.linear_layers:
             x = layer(x)
         return x
+    
+    # Function to add conv blocks
+    def add_conv_block(self, conv_params, max_pool_params):
+        if len(self.conv_layers) >= 5:
+            print("Can only add 5 blocks!")
+            return
+
+        # Handling the case where there are no existing conv layers
+        if not self.conv_layers:
+            in_channels = 1  # Gray Scale 
+        else:
+            in_channels = self.conv_layers[-1].conv.out_channels
+
+        # Adding new Conv Block
+        new_block = ConvBlock(in_channels, **conv_params, maxpool_params=max_pool_params)
+        self.conv_layers.append(new_block)
+
+        # Recalculate the dimensions after adding the new block
+        self._recalculate_dimensions()
+
+        # Update the first linear layer if any linear layers exist
+        if self.linear_layers:
+            flattened = self._calculate_flattened_size()
+            self.linear_layers[0].linear = nn.Linear(flattened, self.linear_layers[0].linear.out_features)
+
+    def _calculate_flattened_size(self):
+        w, h = 28, 28  # Assuming initial image size, adjust if different
+        for layer in self.conv_layers:
+            conv_param = {'kernel_size': layer.conv.kernel_size[0],
+                          'stride': layer.conv.stride[0],
+                          'padding': layer.conv.padding[0]}
+            w, h = self.calculate_output_dims(w, h, **conv_param)
+
+            maxpool_param = {
+                'kernel_size': layer.maxpool.kernel_size,
+                'stride': layer.maxpool.stride,
+                'padding': 0
+            }
+            w, h = self.calculate_output_dims(w, h, **maxpool_param)
+
+        return w * h * self.conv_layers[-1].conv.out_channels if self.conv_layers else 0
+
+    # Function to remove last block
+    def remove_last_block(self):
+        if len(self.conv_layers) > 1:
+            self.conv_layers.pop()
+            self._recalculate_dimensions()
+        else:
+            print("Cannot remove the block. There must be at least one block.")
+
+    def _recalculate_dimensions(self):
+        w, h = 28, 28
+        in_channels = 1
+
+        for layer in self.conv_layers:
+            conv_param = {'kernel_size': layer.conv.kernel_size[0],
+                          'stride': layer.conv.stride[0],
+                          'padding': layer.conv.padding[0]}
+            w, h = self.calculate_output_dims(w, h, **conv_param)
+
+            maxpool_param = {
+                'kernel_size': layer.maxpool.kernel_size,
+                'stride': layer.maxpool.stride,
+                'padding': 0
+            }
+            w,h = self.calculate_output_dims(w,h,**maxpool_param)
+            in_channels = layer.conv.out_channels
+
+        flattened = in_channels * w * h
+
+        if self.linear_layers:
+            self.linear_layers[0].linear.in_features = flattened
+
 
 model = ModelBuilder(conv_params=conv_params,linear_params=linear_params,max_pool_params=maxpool_params)
 print(summary(model, input_size = (1,1,28,28)))
