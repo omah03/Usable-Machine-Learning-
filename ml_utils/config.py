566c984d1b82152f0ec7a4e39fc02ed70e5a1d1e
Lennard Simmons <111833349+SimmonsLZ0N@users.noreply.github.com>
2024-01-18 17:47:07 +0100

    I did it mom

--- ml_utils/config.py
+++ ml_utils/config.py
@@ -15,17 +15,30 @@ conv_params = [
     {"in_channels": 1, "out_channels": 16, "kernel_size_conv": 3, "stride_conv": 1, "padding": 1,}, # Block 1
     {"in_channels": 16, "out_channels": 32, "kernel_size_conv": 3, "stride_conv": 1, "padding": 1 }, # Block 2
     {"in_channels": 32, "out_channels": 64, "kernel_size_conv": 3, "stride_conv": 1, "padding": 1,}, # Block 3
-    {"in_channels": 64, "out_channels": 128, "kernel_size_conv": 3, "stride_conv": 1, "padding": 1,}, # Block 4
-    {"in_channels": 128, "out_channels": 256, "kernel_size_conv": 3, "stride_conv": 1, "padding": 0,} #Block 5
-]
- 
 
+]
+conv_params = {"small":[
+    {"in_channels": 1, "out_channels": 16, "kernel_size_conv": 5, "stride_conv": 1, "padding": 0},
+    {"in_channels": 16, "out_channels": 32, "kernel_size_conv": 3, "stride_conv": 1, "padding": 0},
+    {"in_channels": 32, "out_channels": 64, "kernel_size_conv": 2, "stride_conv": 1, "padding": 0}
+    ],
+    "medium": [
+    {"in_channels": 1, "out_channels": 16, "kernel_size_conv": 8, "stride_conv": 2, "padding": 2},
+    {"in_channels": 16, "out_channels": 32, "kernel_size_conv": 4, "stride_conv": 2, "padding": 0},
+    {"in_channels": 32, "out_channels": 64, "kernel_size_conv": 1, "stride_conv": 1, "padding": 1}
+    ],
+    "large": [
+    {"in_channels": 1, "out_channels": 16, "kernel_size_conv": 10, "stride_conv": 1, "padding":1},
+    {"in_channels": 16, "out_channels": 32, "kernel_size_conv": 6, "stride_conv": 1, "padding":1},
+    {"in_channels": 32, "out_channels": 64, "kernel_size_conv": 3, "stride_conv": 1, "padding":1}
+    ]}
+    
+    
+    
 max_pool_params = [
-    {"kernel_size_max_pool": 2, "stride_max_pool": 2},
-    {"kernel_size_max_pool": 2, "stride_max_pool": 2},
-    {"kernel_size_max_pool": 2, "stride_max_pool": 2},
-    {"kernel_size_max_pool": 1, "stride_max_pool": 1},
-    {"kernel_size_max_pool": 1, "stride_max_pool": 1},
+    {"kernel_size_max_pool": 2, "stride_max_pool": 1},
+    {"kernel_size_max_pool": 2, "stride_max_pool": 1},
+    {"kernel_size_max_pool": 2, "stride_max_pool": 1},
 ]
 
 linear_params = [
